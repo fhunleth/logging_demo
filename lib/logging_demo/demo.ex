fe022faa7e8764dbf7974e076275b52e2ed9bfe4
Frank Hunleth <fhunleth@troodon-software.com>
2018-09-26 17:03:42 -0400

    Updates

--- lib/logging_demo/demo.ex
+++ lib/logging_demo/demo.ex
@@ -2,7 +2,7 @@ defmodule LoggingDemo.Demo do
   use GenServer
   require Logger
 
-  def start_link(args) do
+  def start_link(args \\ []) do
     GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
@@ -11,13 +11,13 @@ defmodule LoggingDemo.Demo do
   end
 
   def init(_args) do
-    Logger.warn("Hello from #{__MODULE__}")
+    Logger.warn("Initialized #{__MODULE__}")
     Process.send_after(self(), :say_something, 10_000)
     {:ok, []}
   end
 
   def handle_info(:say_something, state) do
-    Logger.debug("hello!")
+    Logger.debug("Hello from Demo!")
     Process.send_after(self(), :say_something, 10_000)
     {:noreply, state}
   end
--- lib/logging_demo/evil.ex
+++ lib/logging_demo/evil.ex
@@ -2,7 +2,7 @@ defmodule LoggingDemo.Evil do
   use GenServer
   require Logger
 
-  def start_link(args) do
+  def start_link(args \\ []) do
     GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
@@ -11,18 +11,42 @@ defmodule LoggingDemo.Evil do
   end
 
   def init(_args) do
-    Logger.warn("Hello from #{__MODULE__}")
+    Logger.warn("Initialized #{__MODULE__}")
     Process.send_after(self(), :say_something, delay_millis())
     {:ok, []}
   end
 
   def handle_info(:say_something, state) do
-    Logger.error("oops")
+    Logger.log(rand_level(), say())
     Process.send_after(self(), :say_something, delay_millis())
     {:noreply, state}
   end
 
   defp delay_millis() do
-    1000 + :rand.uniform(5000)
+    500 + :rand.uniform(3000)
+  end
+
+  def rand_level() do
+    Enum.random([:error, :warn, :info, :debug])
+  end
+
+  def say() do
+    "Evil say \"#{rand_message()}\""
+  end
+
+  def rand_message() do
+    Enum.random([
+      "oops",
+      "I might have made a mistake",
+      "this seems important",
+      "debug debug",
+      "did it",
+      "look at me",
+      "surprise",
+      "einval",
+      "authenticated fhunleth, password nerves",
+      "success",
+      "hack hack hack"
+    ])
   end
 end
